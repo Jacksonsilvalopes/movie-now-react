42a139f662bfef2f04b49536453c66e291b591cd
Jackson Silva Lopes <j.a.ckson@hotmail.com>
2025-01-27 10:45:13 -0300

    implantando configuração para host firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAM200s3VHY5UMEZr-6YiNg4bn1PndylV4",
+  authDomain: "movie-now-9cab3.firebaseapp.com",
+  projectId: "movie-now-9cab3",
+  storageBucket: "movie-now-9cab3.firebasestorage.app",
+  messagingSenderId: "233565607958",
+  appId: "1:233565607958:web:43c180f7a8691c7792fd37"
+};
+const app = initializeApp(firebaseConfig);
 
 import App from './App.jsx'
 
